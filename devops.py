59c80f2c07d4452e37c9fb0283880c94eb0417c2
Leo Gomez <leo.gomez@cttexpress.com>
2023-07-11 18:17:16 +0200

    Añadiendo nueva funcionalidad al script devops.py y senderEmail.py

--- devops.py
+++ devops.py
@@ -1,13 +1,25 @@
 import sys
 import os
+import argparse
 
-if len(sys.argv) >= 2:
-  arg1 = sys.argv[1]
-  if arg1 == 'restart':
-    if len(sys.argv) >= 3:
-      if sys.argv[2] in  ['stg', 'prod', 'mstg', 'mpro']:
-        arg2 = sys.argv[2]
-        os.system(f"restart {arg2}")
+parser = argparse.ArgumentParser(prog="DevOps")
+#parser.add_argument("-o", "--opcion", dest = "opcion", help="restart o mail", required=True)
+parser.add_argument("opcion")
+parser.add_argument("-d", "--destinatario", dest = "receiverEmail", help = "Nombre del destinatario")
+parser.add_argument("-s", "--subject", dest = "subject", help = "Cuerpo del subject")
+parser.add_argument("-f", "--file", dest = "html", help = "Archivo html con el cuerpo del mensaje")
+parser.add_argument("-e", "--env", dest = "entorno", help = "entorno")
+args = parser.parse_args()
+arg1 = sys.argv[1]
+if arg1 == 'restart': #args.opcion == 'restart':
+  if len(sys.argv) >= 2:
+    if args.entorno in  ['stg', 'pro', 'mstg', 'mpro']:
+      os.system(f"restart {args.entorno}")
     else:
-      print(f"Agrega un entorno: [stg | pro | mstg | mpro]")
+      print(f"Agrega -e [stg | pro | mstg | mpro]")
+elif arg1 == 'mail': #args.opcion  == 'mail':
+  if len(sys.argv) >= 4:
+    os.system(f"python3 ~/DevOps/tools/senderEmail.py -d {args.receiverEmail} -s \"{args.subject}\" -f \"{args.html}\"")
+  else:
+    print("Agrega -d \"DESTINATARIO\" -s \"SUBJECT\" -f \"FILE\"")
 exit()
--- senderEmail.py
+++ senderEmail.py
@@ -1,47 +1,47 @@
-import smtplib, ssl
+#python3 senderEmail.py -d correoDestinatario -s subject -f html
+import smtplib, ssl, argparse
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-sender_email = "francescgc2010@gmail.com"
-receiver_email = "francescgc2010@gmail.com"
-password = input("Type your password and press enter:")
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--destinatario", dest = "receiverEmail", help = "Nombre del destinatario", required=True)
+parser.add_argument("-s", "--subject", dest = "subject", help = "Cuerpo del subject", required=True)
+parser.add_argument("-f", "--file", dest = "html", help = "Archivo html con el cuerpo del mensaje", required=True)
+args = parser.parse_args()
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "multipart test"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-# Create the plain-text and HTML version of your message
-text = """\
-Hi,
-How are you?
-Real Python has many great tutorials:
-www.realpython.com"""
-html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       How are you?<br>
-       <a href="http://www.realpython.com">Real Python</a> 
-       has many great tutorials.
-    </p>
-  </body>
-</html>
-"""
-
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
-
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
-
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+if args.receiverEmail is not None:
+  sender_email = "francescgc2010@gmail.com"
+  receiver_email = args.receiverEmail
+  password = input("Escribe tu contraseña aquí: ")
+  
+  message = MIMEMultipart("alternative")
+  message["Subject"] = args.subject
+  message["From"] = sender_email
+  message["To"] = receiver_email
+  html = args.html
+  
+  # Create the plain-text and HTML version of your message
+  text = """\
+  Hi,
+  How are you?
+  Real Python has many great tutorials:
+  www.realpython.com"""
+  
+  # Turn these into plain/html MIMEText objects
+  part1 = MIMEText(text, "plain")
+  part2 = MIMEText(html, "html")
+  
+  # Add HTML/plain-text parts to MIMEMultipart message
+  # The email client will try to render the last part first
+  message.attach(part1)
+  message.attach(part2)
+  
+  # Create secure connection with server and send email
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          sender_email, receiver_email, message.as_string()
+      )
+else:
+  print("Utiliza --help para saber como funciona el comando")
